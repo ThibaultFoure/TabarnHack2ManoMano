4d6158e83d657a3a58d6d0865a771d708f8f6081
Djoa <siongdjoa3@gmail.com>
2022-01-20 19:24:11 +0100

    change name

--- src/DataFixtures/StyleFixtures.php
+++ src/DataFixtures/StyleFixtures.php
@@ -12,19 +12,19 @@ class StyleFixtures extends Fixture
     const STYLES = [
         [
             'name' => 'Moderne',
-            'image' => 'salledebain1.jpg',
+            'image' => 'salledebain3.jpg',
             'categorie' => 'categorie_0',
             'description' => 'Envie de design dans votre salle de bain ? Le style moderne conviendra à vos attentes. Il apportera à votre pièce une touche de sérénité et de détente.'
         ],
         [
-            'name' => 'Bohème',
-            'image' => 'salledebain2.jpg',
+            'name' => 'Palais royal',
+            'image' => 'salledebain1.jpg',
             'categorie' => 'categorie_0',
-            'description' => "La déco bohème est un style incontournable et intemporel, qui continue de séduire les adeptes de décoration d’intérieur. Avec son mix de différentes ambiances, il nous fait voyager tout en transformant la pièce dans laquelle on l’invite."
+            'description' => "L’or brossé est véritablement la matière tendance pour décorer votre salle de bain. La robinetterie dorée, il fallait y penser ! Pourtant, là réside tout le charme d’une salle de bain décorée avec style."
         ],
         [
-            'name' => 'Rustique',
-            'image' => 'salledebain3.jpg',
+            'name' => 'rustique',
+            'image' => 'salledebain2.jpg',
             'categorie' => 'categorie_0',
             'description' => 'Le charme du rustique dans la salle de bain apporte une touche chaleureuse et confortable, sans oublier de donner un caractère unique à cet espace, notamment avec le mélange des textures et des matériaux comme le bois, la pierre, le métal.'
         ],
@@ -36,13 +36,13 @@ class StyleFixtures extends Fixture
         ],
         [
             'name' => 'Classique',
-            'image' => 'salledebain5.jpg',
+            'image' => 'salledebain6.jpg',
             'categorie' => 'categorie_0',
             'description' => "Améliorez le standing de votre salle de bains tout en délicatesse en choisissant le style classique chic : un subtil mélange entre l’ancien et le contemporain qui donne à la pièce un cachet intemporel."
         ],
         [
             'name' => 'Vintage',
-            'image' => 'salledebain6.jpg',
+            'image' => 'salledebain5.jpg',
             'categorie' => 'categorie_0',
             'description' => 'Si vous cherchez à mettre à jour votre salle de bain, pourquoi ne pas envisager un look vintage? Ce style est idéal pour ceux qui recherchent une salle de bain avec beaucoup de charme et de caractère.'
         ],
