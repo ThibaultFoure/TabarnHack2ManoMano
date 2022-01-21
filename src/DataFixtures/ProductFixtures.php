6bc8d447f459e7fe60f6c2740c8fdaae17f38289
Thibault <thibaultfoure@gmail.com>
2022-01-21 10:24:58 +0100

    new products

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -57,7 +57,57 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
             'image' => 'objet4.png',
             'price' => 184,
             'description' => 'Robinet en or',
-        ],
+        ], [
+            'name' => 'Robinet',
+            'image' => 'baignoiremetal.webp',
+            'price' => 123,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'baignoireor.jpg',
+            'price' => 843,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'bougeoir.jpg',
+            'price' => 42,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'bougeoirbois.webp',
+            'price' => 39,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'miroircuivre.jpg',
+            'price' => 260,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'miroirmetal.jpg',
+            'price' => 195,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'poubelledore.jpg',
+            'price' => 68,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'poubelleplastique.JPG',
+            'price' => 96,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'robinetcristal.jpg',
+            'price' => 149,
+            'description' => 'Robinet en or',
+        ], [
+            'name' => 'Robinet',
+            'image' => 'robinetor.jpg',
+            'price' => 128,
+            'description' => 'Robinet en or',
+        ]
     ];
 
     public function load(ObjectManager $manager): void
@@ -73,7 +123,7 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
                 __DIR__ . '/../../public/uploads/styles/' . $productName['image']
             );
             $product->setCategory($this->getReference('categorie_0'));
-            $product->setStyle($this->getReference('style_' . rand(0,5)));
+            $product->setStyle($this->getReference('style_' . rand(0, 5)));
             $manager->persist($product);
         }
         $manager->flush();
